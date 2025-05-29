7bf6099d324fa8e63ecee91cd2af0f1757c4b1e6
DKaff <dkassay1@gmail.com>
2025-05-29 17:19:08 -0400

    Updated Render URL

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const nodemailer = require('nodemailer');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'web-l0wfx3qna-corvuxs-projects.vercel.app', // <-- replace with your actual Vercel frontend URL
+}));
 app.use(express.json());
 
 app.post('/api/contact', async (req, res) => {
